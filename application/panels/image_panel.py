dc1dc62cdc233ff261607c8f6194da95981c5b7c
vlad <mvdoster@gmail.com>
2019-08-03 03:18:00 -0400

    Dataset creator addition

--- application/panels/image_panel.py
+++ application/panels/image_panel.py
@@ -1,3 +1,5 @@
+import os
+
 import wx
 
 
@@ -8,37 +10,46 @@ class ImageStreamPanel(wx.StaticBox):
         self.Init_UI()
 
     def Init_UI(self):
+        # Image Stream info box
+        ######################################
         self.panel = wx.StaticBoxSizer(self, wx.HORIZONTAL)
-        self.choices_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.raw_stream_choice = wx.RadioButton(self.GetParent(), label='Raw', pos=(10, 10),
-                                                style=wx.RB_GROUP)
-        self.classification_choice = wx.RadioButton(self.GetParent(), label='Image classification', pos=(10, 30))
-        self.obj_detection_choice = wx.RadioButton(self.GetParent(), label='Object detection', pos=(10, 50))
 
-        self.raw_stream_choice.Bind(wx.EVT_RADIOBUTTON, self.SetVal)
-        self.classification_choice.Bind(wx.EVT_RADIOBUTTON, self.SetVal)
-        self.obj_detection_choice.Bind(wx.EVT_RADIOBUTTON, self.SetVal)
+        # Stream options input
+        ######################################
+        self.data_set_creator_input_panel = wx.StaticBox(self.GetParent(), label="Dataset Creator")
+        self.data_set_creator_input_sizer = wx.StaticBoxSizer(self.data_set_creator_input_panel, wx.VERTICAL)
+        self.start_data_set_creator = wx.Button(self.GetParent().GetParent(), label='Start recording', pos=(10, 50))
+        self.start_data_set_creator.Bind(wx.EVT_BUTTON, self.button_browse_path_click)
+        self.save_data_set_to_dir = wx.StaticText(self.GetParent(), -1)
 
+        self.images_capture_rate_label = wx.StaticText(self.GetParent(), -1, "Images captured per second")
+        self.images_capture_rate = wx.SpinCtrl(self.GetParent(), min=1)
+        self.save_data_set_to_dir.Wrap(width=10)
+        self.data_set_creator_input_sizer.Add(self.start_data_set_creator, 0, wx.ALL | wx.CENTER, 5)
+        self.data_set_creator_input_sizer.Add(self.save_data_set_to_dir, 0, wx.ALL, 5)
+        self.data_set_creator_input_sizer.Add(self.images_capture_rate_label, 0, wx.ALL | wx.CENTER, 5)
+        self.data_set_creator_input_sizer.Add(self.images_capture_rate, 0, wx.ALL | wx.CENTER, 5)
+
+        ######################################
+
+        # Image stream from AUV
+        ######################################
         self.raw_image_sizer = wx.BoxSizer(wx.VERTICAL)
+        # This can be checked in project/camera_publisher.py
         self.blank_img = wx.Image(320, 240)
         self.raw_camera_stream = wx.StaticBitmap(self.GetParent(), wx.ID_ANY,
                                                  wx.Bitmap(self.blank_img))
-
-        self.choices_sizer.Add(self.raw_stream_choice, 0, wx.ALL, 5)
-        self.choices_sizer.Add(self.classification_choice, 0, wx.ALL, 5)
-        self.choices_sizer.Add(self.obj_detection_choice, 0, wx.ALL, 5)
-
-        self.panel.Add(self.choices_sizer, 0, wx.ALL, 5)
         self.current_image_stream_title = wx.StaticText(self.GetParent(), -1, "Raw image stream")
         self.raw_image_sizer.Add(self.raw_camera_stream, 0, wx.ALL)
         self.raw_image_sizer.Add(self.current_image_stream_title, 0, wx.ALL | wx.ALIGN_CENTER, 5)
-        self.raw_image_sizer.Hide(self.current_image_stream_title)
+        ######################################
+
+        self.panel.Add(self.data_set_creator_input_sizer, 0, wx.ALL, 5)
         self.panel.Add(self.raw_image_sizer, 0, wx.ALL, 5)
 
-    def SetVal(self, e):
-        if str(self.raw_stream_choice.GetValue()):
-            self.current_image_stream_title.SetLabel("Raw stream")
-        if str(self.classification_choice.GetValue()):
-            self.current_image_stream_title.SetLabel("Image classification")
-        if str(self.obj_detection_choice.GetValue()):
-            self.current_image_stream_title.SetLabel("Object detection")
+    def button_browse_path_click(self, event):
+        print("Starting to record image dataset")
+        print(os.listdir("captured_datasets"))
+
+        self.save_data_set_to_dir.Wrap(width=10)
+        self.save_data_set_to_dir.Update()
