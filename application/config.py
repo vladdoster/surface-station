1e40e2446fcda5d85324cb27b2f66ea2a2893951
vlad <mvdoster@gmail.com>
2019-08-11 20:52:34 -0400

    added more config options

--- application/config.py
+++ application/config.py
@@ -8,12 +8,20 @@ splash_screen_time = 5000  # In milliseconds
 
 # dev
 dev_camera_ws_url = "ws://127.0.0.1:9000"
+dev_camera_ws_port = 9000
+
 dev_joystick_ws_url = "ws://127.0.0.1:9001"
+dev_joystick_ws_port = 9001
+
 dev_robot_url = "127.0.0.1"
 
 # live
 live_camera_ws_url = "ws://enbarr.local:9000"
+live_camera_ws_port = 9000
+
 live_joystick_ws_url = "ws://enbarr.local:9001"
+live_joystick_ws_port = 9001
+
 live_robot_url = "enbarr.local"
 
 # Google cloud
